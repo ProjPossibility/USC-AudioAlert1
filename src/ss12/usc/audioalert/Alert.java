af8904594d6599623cbd018275b7c38bc0d5e03b
Cathy <Cathy@Cathy-PC>
2013-02-02 14:39:24 -0800

    Added audio input

--- src/ss12/usc/audioalert/Alert.java
+++ src/ss12/usc/audioalert/Alert.java
@@ -2,9 +2,13 @@ package ss12.usc.audioalert;
 
 import android.app.Activity;
 import android.content.Context;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder.AudioSource;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.Menu;
+
 
 public class Alert extends Activity {
 	
@@ -14,10 +18,27 @@ public class Alert extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_alert);
 		
+//****************//		
+		int channel_config = AudioFormat.CHANNEL_IN_MONO;
+		int format = AudioFormat.ENCODING_PCM_16BIT;
+		int sampleSize = 8000;
+		int bufferSize = AudioRecord.getMinBufferSize(sampleSize, channel_config, format);
+		AudioRecord audioInput = new AudioRecord(AudioSource.MIC, sampleSize, channel_config, format, bufferSize);
+
+		
+		short[] audioBuffer = new short[bufferSize];
+		audioInput.startRecording();
+		audioInput.read(audioBuffer, 0, bufferSize);
+		
+//****************//
+		
 		Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
 		long[] _pattern = new long[]{0,300,0,300,0,300};
 		v.vibrate(_pattern, -1);
+		
+		
+		
 	}
 
 	@Override
--- src/ss12/usc/audioalert/MainActivity.java
+++ src/ss12/usc/audioalert/MainActivity.java
@@ -32,4 +32,6 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    
 }
+
