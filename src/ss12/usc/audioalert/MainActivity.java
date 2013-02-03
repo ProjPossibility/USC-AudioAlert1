823692d9dac5b16e3a62545f94c1c514a8d8067e
Douglass Chen <douglasschen@gmail.com>
2013-02-02 17:06:42 -0800

    Testing to check if initializing AudioRecord object works

--- src/ss12/usc/audioalert/MainActivity.java
+++ src/ss12/usc/audioalert/MainActivity.java
@@ -2,8 +2,15 @@ package ss12.usc.audioalert;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder.AudioSource;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+
+// code for checking for finding a valid AudioRecord comes from this source:
+// http://stackoverflow.com/questions/4843739/audiorecord-object-not-initializing
 
 public class MainActivity extends Activity {
 	
@@ -14,16 +21,41 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        /* @SuppressWarnings("deprecation")
 		int channel_config = AudioFormat.CHANNEL_CONFIGURATION_MONO;
 		int format = AudioFormat.ENCODING_PCM_16BIT;
-		int sampleSize = 8000;
-		int bufferSize = AudioRecord.getMinBufferSize(sampleSize, channel_config, format);
-		AudioRecord audioInput = new AudioRecord(AudioSource.MIC, sampleSize, channel_config, format, bufferSize);
+		int[] mSampleRates = new int[]{8000, 11025, 22050, 44100};
+		AudioRecord recorder = null;
 		
-		short[] audioBuffer = new short[bufferSize];
-		audioInput.startRecording();
-		audioInput.read(audioBuffer, 0, bufferSize); */
+		for (int rate : mSampleRates) {
+	        for (short audioFormat : new short[] { AudioFormat.ENCODING_PCM_8BIT, AudioFormat.ENCODING_PCM_16BIT }) {
+	            for (short channelConfig : new short[] { AudioFormat.CHANNEL_IN_MONO, AudioFormat.CHANNEL_IN_STEREO }) {
+	                try {
+	                    Log.d("MainActivity", "Attempting rate "+rate+"Hz, bits: "+audioFormat+", channel: "+channelConfig);
+	                    int bufferSize = AudioRecord.getMinBufferSize(rate, channelConfig, audioFormat);
+
+	                    if (bufferSize != AudioRecord.ERROR_BAD_VALUE) {
+	                        // check if we can instantiate and have a success
+	                        recorder = new AudioRecord(AudioSource.DEFAULT, rate, channelConfig, audioFormat, bufferSize);
+	                        if (recorder.getState() != AudioRecord.STATE_INITIALIZED)
+	                            recorder = null;
+	                    }
+	                } catch (Exception e) {
+	                    Log.e("MainActivity", rate + "Exception, keep trying.",e);
+	                }
+	            }
+	        }
+	    }
+		if(recorder != null)
+		{
+			int sampleSize = recorder.getSampleRate();
+			int bufferSize = AudioRecord.getMinBufferSize(sampleSize, channel_config, format);
+			AudioRecord audioInput = new AudioRecord(AudioSource.MIC, sampleSize, channel_config, format, bufferSize);
+			
+			short[] audioBuffer = new short[bufferSize];
+			audioInput.startRecording();
+			audioInput.read(audioBuffer, 0, bufferSize);
+			Log.d("MainActivity", "LOLOLOL IT WORKS :D");
+		}
     }
     
     public void sendMessage() {
